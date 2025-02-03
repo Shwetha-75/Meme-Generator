4cb2e088dd295d555f431d08adcec894782d3cb4
Shwetha K <shweetha.sweety17@gmail.com>
2025-02-03 16:02:38 +0530

    25-02-03_Meme_Generator_[Shwetha K]_:_Resolved the issues on on changing the input field & captured x & y port for the motion tag

--- src/Component/FormComp.jsx
+++ src/Component/FormComp.jsx
@@ -2,58 +2,99 @@ import React from 'react'
 import "./form.css";
 import { motion } from "framer-motion";
 import SelectedImage from './UseContextImage';
+import { nanoid } from 'nanoid';
 // import { nanoid } from 'nanoid';
 export default function FormComp() {
   const constraintsRef = React.useRef(null);
   const [editableTextTag,setEditableTextTag]=React.useState([]);
   const [counter,setCounter]=React.useState(0);
+  const [data,setData]=React.useState([]);
+  const [selected,setSelected]=React.useState("");
 
+  // creating the text for each input field 
+  // const handleOnChange=(event)=>{
+        
+  // }
 
+  
+  const handleSelectedDataToRemove=(id)=>{
+    setSelected(id)
+  }
+  
   React.useEffect(()=>{
     const data_set=[]
-    for(let i=0;i<counter;i++){
-      data_set.push(
-        <motion.div 
-        key={i}
+      data.map((item)=>
+        
+        data_set.push(
+          <motion.div 
+        onClick={()=>handleSelectedDataToRemove()}
+        key={item.id}
+        id={`text--${item.id}`}
         drag
         dragConstraints={constraintsRef}
         dragElastic={0.2}
-       
-        className="box--image"
-      >
+        className="box--image mt-[5%]">
         
-        <input 
-           
+        <input  
            className='input--tag'
+           name={`text-${item.id}`}
+           
            type="text"
            />
         </motion.div>
       )
-    }
-
+    )
+    
+    
+    
     setEditableTextTag(data_set);
-  },[editableTextTag,counter])
+  },[setEditableTextTag,counter,data]);
+
+
+  React.useEffect(()=>{
+    data.map((item)=>
+        document.querySelector(`#text--${item.id}`)?.addEventListener('mousemove',function(){
+      // console.log(document.querySelector(`#text--${item.id}`)?.getBoundingClientRec
+      // 
+      //   ().left+"---"+document.querySelector(`#text--${item.id}`)?.getBoundingClientRect().top)
+        let x=this?.getBoundingClientRect().left;
+        let y=this?.getBoundingClientRect().top;
+        console.log(x+"--"+y)
+        })
+
+    )
+  })
 
 
   const handleOnClick=()=>{
     if(counter<0){
       setCounter(0)
     }
+   
+
+    setData(prev=>
+    [...prev,{
+      id:nanoid(),
+      value:''
+    }]
+    )
     setCounter(prev=>prev+1);
   }
   const handleOnClickRemove=()=>{
     setCounter(prev=>prev-1);
-
+    data.pop();
   }
+  
+  
   const selectedImage=React.useContext(SelectedImage);
- console.log(selectedImage.selectedImage)
+
   return (
     <div className='mt-10 h-[350px] h-[500px]  w-[100%] flex position-absolute border border-sky-500'>
   
 <div className='w-[20%] border border-sky-500 ml-[5%] mt-[10px] mb-[10px]'>
 
 <div className="w-[90%] ml-[5%] p-[10px] mt-[5px] btn---text--area border border-sky-500">
-  <p className='text-white text-center ' onClick={handleOnClick}>Add</p>
+  <p className='text-white text-center ' onClick={handleOnClick} id="add--tag">Add</p>
   </div>
   
   <div className="w-[90%] ml-[5%] p-[10px] mt-[5px] btn---text--area border border-sky-500">
@@ -69,10 +110,6 @@ export default function FormComp() {
   {editableTextTag}
 
   </div>
-  
- 
-
-
     </div>
   )
 }
--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -2,11 +2,28 @@ import React from 'react'
 import Card from './Card';
 import FormComp from './FormComp';
 import SelectedImage from './UseContextImage';
-
+// import { saveAs } from 'file-saver';
 // creating an use context to store the value of the image 
 
 
 export default function Main() {
+  // const handleOnClickImage=async(image)=>{
+  //   // passing direct public url of the image 
+  //   // but instead we should pass storage image url or the image stored in any storage devices like we have blob default 
+
+  //     // saveAs(image,"Image-download")
+
+  //     try{
+  //          console.log(image)
+  //       let response=await fetch(image);
+  //       console.log(response)
+  //       let image_url=await response.blob();
+  //       saveAs(image_url,"image-download.jpg");
+
+  //     }catch(error){
+  //       console.log("Error while downloading the image ")
+  //     }
+  // }
  
   const [selectedImage,setSelectedImage]=React.useState(JSON.parse(localStorage.getItem('selectedImage'))||{});
 
@@ -20,6 +37,7 @@ export default function Main() {
     <div className='w-[90%] ml-[5%]'>
         <Card/>
         <FormComp/>
+       
     </div>
     </SelectedImage.Provider >
   )
